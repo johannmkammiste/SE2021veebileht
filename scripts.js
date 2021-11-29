c952ba831427288e184165911a8a4d961e8bca30
Johann Mattias Kammiste <johannmkammiste@gmail.com>
2021-11-29 21:45:17 +0200

    Updated CSS (thanks Lauri!)

--- scripts.js
+++ scripts.js
@@ -4,10 +4,11 @@ window.onload = function() {
     var month = d.getMonth() + 1;
     var year = d.getFullYear();
     var dateeur = day+'.'+month+'.'+year;
-    document.getElementById('heading').innerHTML =  'Top 15 Memes of Reddit on ' + dateeur;
+    document.getElementById('logo').innerHTML = dateeur;
 }
-
-const container = document.querySelector(".post-container");
+const container1 = document.querySelector(".post-container1");
+const container2 = document.querySelector(".post-container2");
+const container3 = document.querySelector(".post-container3");
 const renderPosts = (postType) => {
 
   fetch(`https://www.reddit.com/r/Memes/${postType}.json`)
@@ -23,15 +24,16 @@ const renderPosts = (postType) => {
       const postsArr = res.data.children;
 
       // Add a title based on post type
-      markup = `<h3>${postType} posts from r/Memes</h3>`;
+      markup = `<h3 class="heading">${postType} posts from r/Memes</h3>`;
 
       // Iterate through our posts array and chain
       // the markup based on our HTML structure
       for (let i = 0; i < 5; i++) {
+        var currentpos = +i + +1
         currPost = postsArr[i].data; // a single post object
         markup += `
           <a class="post" href="https://www.reddit.com/${currPost.permalink}">
-            <div class="title"> ${currPost.title} </div>
+            <div class="title">top ${currentpos}. ${currPost.title} </div>
             <div class="content"> 
               ${currPost.selftext}
               </br></br>
@@ -41,34 +43,30 @@ const renderPosts = (postType) => {
           </a>`;
       }
       // Insert the markup HTML to our container
-      container.insertAdjacentHTML('afterbegin', markup);
+      container1.insertAdjacentHTML('afterbegin', markup);
     })
     .catch(function(err) {
       console.log(err); // Log error if any
     });
 
-    fetch(`https://www.reddit.com/r/terriblefacebookmemes/${postType}.json`)
+  fetch(`https://www.reddit.com/r/terriblefacebookmemes/${postType}.json`)
     .then(function(res) {
       // Return the response in JSON format
       return res.json();
     })
     .then(function(res) {
-      // We render our posts to the UI in this block
       let currPost, markup = ``;
 
-      // The array that contains our posts
       const postsArr = res.data.children;
 
-      // Add a title based on post type
-      markup = `<h3>${postType} posts from r/terriblefacebookmemes</h3>`;
+      markup = `<h3 class="heading">${postType} posts from r/terriblefacebookmemes</h3>`;
 
-      // Iterate through our posts array and chain
-      // the markup based on our HTML structure
       for (let i = 0; i < 5; i++) {
-        currPost = postsArr[i].data; // a single post object
+        var currentpos = +i + +1
+        currPost = postsArr[i].data;
         markup += `
           <a class="post" href="https://www.reddit.com/${currPost.permalink}">
-            <div class="title"> ${currPost.title} </div>
+          <div class="title">top ${currentpos}. ${currPost.title} </div>
             <div class="content"> 
               ${currPost.selftext} 
               </br></br>
@@ -77,35 +75,28 @@ const renderPosts = (postType) => {
             <div class="author"> Posted by ${currPost.author} </div>
           </a>`;
       }
-      // Insert the markup HTML to our container
-      container.insertAdjacentHTML('afterbegin', markup);
+      container2.insertAdjacentHTML('afterbegin', markup);
     })
     .catch(function(err) {
-      console.log(err); // Log error if any
+      console.log(err); 
     });
 
-    fetch(`https://www.reddit.com/r/sbubby/${postType}.json`)
+  fetch(`https://www.reddit.com/r/sbubby/${postType}.json`)
     .then(function(res) {
-      // Return the response in JSON format
       return res.json();
     })
     .then(function(res) {
-      // We render our posts to the UI in this block
       let currPost, markup = ``;
 
-      // The array that contains our posts
       const postsArr = res.data.children;
+      markup = `<h3 class="heading">${postType} posts from r/sbubby</h3>`;
 
-      // Add a title based on post type
-      markup = `<h3>${postType} posts from r/sbubby</h3>`;
-
-      // Iterate through our posts array and chain
-      // the markup based on our HTML structure
       for (let i = 0; i < 5; i++) {
-        currPost = postsArr[i].data; // a single post object
+        var currentpos = +i + +1
+        currPost = postsArr[i].data;
         markup += `
           <a class="post" href="https://www.reddit.com/${currPost.permalink}">
-            <div class="title"> ${currPost.title} </div>
+          <div class="title">top ${currentpos}. ${currPost.title} </div>
             <div class="content"> 
               ${currPost.selftext} 
               </br></br>
@@ -114,12 +105,11 @@ const renderPosts = (postType) => {
             <div class="author"> Posted by ${currPost.author} </div>
           </a>`;
       }
-      // Insert the markup HTML to our container
-      container.insertAdjacentHTML('afterbegin', markup);
+      container3.insertAdjacentHTML('afterbegin', markup);
     })
     .catch(function(err) {
-      console.log(err); // Log error if any
+      console.log(err);
     });
 };
-// Load hot posts on page load
+// Load top posts on page load
 renderPosts("top");
